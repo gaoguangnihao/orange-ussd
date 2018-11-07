07630eb2627b8c7d59e508b1b5ee1e8efaf8560b
Guang.Gao <Guang.Gao@kaiostech.com>
2018-11-07 14:28:22 +0800

    config for my-orange

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 
 
 class Config {
-  KEY_USSDSTR = 'orange.money';
-  ussdStr = '61302,*144#;65202,*145#;63086,*144#;63089,*144#;62303,#144#;61203,#144#;62402,#150#;60201,#115#;63203,#144#;61101,#144#*144#;41677, ;61807,*144#;60400, ;61002,#144#;64602,#144#;61404,#144#;61901,#144#;60801,#144#;60501,*139#;';
+  KEY_USSDSTR = 'orange.ussds';
+  ussdStr = '65202,http://myorange.orange.co.bw/myorangewebapp/MyOrange-Botswana/;62402,http://myorange.orange.cm/myorangewebapp/MyOrange-Cameroon/;61203,https://espaceclient.orange.ci/;61101,http://myorange.orange-guinee.com/myorangewebapp/MyOrange-Guinee/;41677,http://ssowt-zr.orange.jo/myorangewebapp/MyOrange-Jordan/;64602,http://zasyorange.orange.mg/myorangewebapp/MyOrange-Madagascar/index.html;60400,https://espace-client.orange.ma/sso/login;61404,http://myorange.orange.ne/myorangewebapp/MyOrange-Niger/;63086,http://myorange.orange.cd/myorangewebapp/MyOrange-CongoDemocratique/;63089,http://myorange.orange.cd/myorangewebapp/MyOrange-CongoDemocratique/;60801,http://myorange.orange.sn/myorangewebapp/MyOrange-Senegal/;61302,;62303,;60201,;63203,;61807,;61002,;61901,;60501,;';
 }
 
 const config = new Config();
