cbdaeafd7fc6e1614399e84012fdcedba87f430a
Guang.Gao <Guang.Gao@kaiostech.com>
2018-11-13 18:19:15 +0800

    Change config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 class Config {
   KEY_USSDS = 'orange.ussds';
   ussds = {  
-    "65202": "http: //myorange.orange.co.bw/myorangewebapp/MyOrange-Botswana/",
-    "62402": "http: //myorange.orange.cm/myorangewebapp/MyOrange-Cameroon/",
+    "65202": "http://myorange.orange.co.bw/myorangewebapp/MyOrange-Botswana/",
+    "62402": "http://myorange.orange.cm/myorangewebapp/MyOrange-Cameroon/",
     "61203": "https://espaceclient.orange.ci/",
-    "61101": "http: //myorange.orange-guinee.com/myorangewebapp/MyOrange-Guinee/",
+    "61101": "http://myorange.orange-guinee.com/myorangewebapp/MyOrange-Guinee/",
     "41677": "http://ssowt-zr.orange.jo/myorangewebapp/MyOrange-Jordan/",
-    "64602": "http: //zasyorange.orange.mg/myorangewebapp/MyOrange-Madagascar/index.html",
-    "60400": "https: //espace-client.orange.ma/sso/login",
+    "64602": "http://zasyorange.orange.mg/myorangewebapp/MyOrange-Madagascar/index.html",
+    "60400": "https://espace-client.orange.ma/sso/login",
     "61404": "http://myorange.orange.ne/myorangewebapp/MyOrange-Niger/",
     "63086": "http://myorange.orange.cd/myorangewebapp/MyOrange-CongoDemocratique/",
     "63089": "http://myorange.orange.cd/myorangewebapp/MyOrange-CongoDemocratique/",
